b9be255bf648d7138413fa5480bff1c66ec8f76d
AmolShelke2 <shelkeamol897@gmail.com>
2024-03-21 00:18:07 +0530

    [feat]: font-changed and react-icons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import logo from "../assets/crework-logo.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,19 +23,12 @@ export const Header = () => {
           <div className="block lg:hidden">
             <button
               onClick={handleMenuToggle}
-              className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+              className="text-[#BDBCBC] hover:text-gray-500 focus:outline-none focus:text-gray-500">
+              {menuOpen ? (
+                <AiOutlineClose size={25} />
+              ) : (
+                <AiOutlineMenu size={25} />
+              )}
             </button>
           </div>
 
@@ -42,12 +36,18 @@ export const Header = () => {
           <ul
             className={`${
               menuOpen
-                ? "block absolute top-16 bg-black w-full left-0 z-10"
+                ? "flex gap-6 py-2 absolute top-16 bg-black w-full left-0 z-10 my-2"
                 : "hidden"
-            } lg:flex lg:flex-row lg:items-center lg:gap-8 text-[#BDBCBC]`}>
-            <li className="font-normal leading-6 text-xl">30 Days of PM</li>
-            <li className="font-normal leading-6 text-xl">Newsletter</li>
-            <li className="font-normal leading-6 text-xl">Builders Cohort</li>
+            } lg:flex flex-col items-center lg:flex-row lg:items-center lg:gap-8 text-[#BDBCBC]`}>
+            <li className="font-normal leading-6 text-xl cursor-pointer">
+              30 Days of PM
+            </li>
+            <li className="font-normal leading-6 text-xl cursor-pointer">
+              Newsletter
+            </li>
+            <li className="font-normal leading-6 text-xl cursor-pointer">
+              Builders Cohort
+            </li>
           </ul>
         </div>
       </nav>
